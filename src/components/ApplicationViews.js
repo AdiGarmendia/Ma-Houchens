3004ca34d9e0c2c810a63203c55eba6537099fb0
Adrian Garmendia <dejima@gmail.com>
2020-06-16 10:25:14 -0500

    Added login component

---
+++ src/components/ApplicationViews.js
@@ -0,0 +1,25 @@
+import { Route } from "react-router-dom";
+import React from "react";
+import Home from "./home/Home";
+
+const ApplicationViews = () => {
+	return (
+		<React.Fragment>
+			<Route
+				exact
+				path="/"
+				render={(props) => {
+					return <Home />;
+				}}
+			/>
+			<Route
+				path="/login"
+				render={(props) => {
+					return <Login setUser={setUser} {...props} />;
+				}}
+			/>
+		</React.Fragment>
+	);
+};
+
+export default ApplicationViews;
