1df5bd56cb0e458bfb24f39094c01b401a15174e
Adrian Garmendia <dejima@gmail.com>
2020-06-24 14:53:04 -0500

    chart now renders from data acquired from a fetch

--- src/components/charts/Chart.js
+++ src/components/charts/Chart.js
@@ -1,55 +1,70 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Pie } from "react-chartjs-2";
 import OffenderManager from "../../modules/OffenderManager";
 
-// const offenseData = (props) => {
-// 	const [offenses, setOffenses] = useState([]);
+const Chart = (props) => {
+	const [offenseCount, setOffenseCount] = useState([0, 0]);
+	// useEffect takes 2 arguements. 1 a function, 2 a array values
+	useEffect(() => {
+		const getOffenders = () => {
+			return OffenderManager.getAll().then((offenders) => {
+				let tempArray = [...offenseCount];
+				offenders.forEach((offender) => {
+					if (offender.locationId === "1") {
+						tempArray[0]++;
+					} else {
+						tempArray[1]++;
+					}
+				});
+				console.log(tempArray);
+				setOffenseCount(tempArray);
+			});
+		};
+		getOffenders();
+	}, []);
 
-// 	const getOffenders = () => {
-// 		return OffenderManager.getAll().then((offendersFromAPI) => {
-// 			setOffenses(offendersFromAPI.locationId);
-// 		});
-// 	};
+	const storeData = {
+		labels: ["Glasgow", "Portland"],
+		datasets: [
+			{
+				label: "Offenses",
+				backgroundColor: [
+					"#B21F00",
+					"#C9DE00",
+					"#2FDE00",
+					"#00A6B4",
+					"#6800B4",
+				],
+				hoverBackgroundColor: [
+					"#501800",
+					"#4B5000",
+					"#175000",
+					"#003350",
+					"#35014F",
+				],
+				data: offenseCount,
+			},
+		],
+	};
 
-const data = {
-	labels: ["Glasgow", "Portland"],
-	datasets: [
-		{
-			label: "Offenses",
-			backgroundColor: ["#B21F00", "#C9DE00", "#2FDE00", "#00A6B4", "#6800B4"],
-			hoverBackgroundColor: [
-				"#501800",
-				"#4B5000",
-				"#175000",
-				"#003350",
-				"#35014F",
-			],
-			data: [3, 6],
-		},
-	],
+	return (
+		<div>
+			<Pie
+				data={storeData}
+				options={{
+					title: {
+						display: true,
+						text: "Offenses Per Store",
+						fontSize: 20,
+					},
+					legend: {
+						display: true,
+						position: "right",
+					},
+				}}
+			/>
+		</div>
+	);
 };
-
-class Chart extends React.Component {
-	render() {
-		return (
-			<div>
-				<Pie
-					data={data}
-					options={{
-						title: {
-							display: true,
-							text: "Offenses Per Store",
-							fontSize: 20,
-						},
-						legend: {
-							display: true,
-							position: "right",
-						},
-					}}
-				/>
-			</div>
-		);
-	}
-}
 
 export default Chart;
